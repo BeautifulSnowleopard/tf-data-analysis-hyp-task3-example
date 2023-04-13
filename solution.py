d1cadd78b742372a6c53ba9803664cb814c475b7
artist_mary <94290722+BeautifulSnowleopard@users.noreply.github.com>
2023-04-13 23:23:31 +0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,6 +5,9 @@ from scipy.stats import ttest_ind
 chat_id = 1897874711 # Ваш chat ID, не меняйте название переменной
 
 def solution(control: np.ndarray, test: np.ndarray) -> bool:
-    alpha = 0.09
-    result = ttest_ind(control, test)
-    return result.pvalue/2 < alpha and result.statistic > 0
+    alpha = 0.09 # уровень значимости критерия
+    stat, p_value = ttest_ind(control, test, equal_var=False) # двухвыборочный t-критерий Стьюдента
+    if p_value < alpha:
+        return True # отклоняем нулевую гипотезу
+    else:
+        return False # не отклоняем нулевую гипотезу
